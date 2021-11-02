5ab25d368c8f7ffa06d1a39d61986dff2f8a7c68
CottonEyeJoey <joedel1310@gmail.com>
2021-11-02 18:45:42 +0200

    Sorted Layering for application

--- PhotoAlbumSystem/LogicLayer/Implementations/AddImpl.cs
+++ PhotoAlbumSystem/LogicLayer/Implementations/AddImpl.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataLayer;
+using DataLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,22 @@ namespace LogicLayer.Implementations
 {
     public class AddImpl
     {
+        private readonly DatabaseContext ourDatabase;
+        public AddImpl(DatabaseContext db) 
+        {
+            ourDatabase = db;
+        }
+        public Photo AddPhoto(string FileName, Guid Album_Id)
+        {
+            var photo = new Photo()
+            {
+                Photo_Id = Guid.NewGuid(),
+                FileName = FileName,
+                Album_Id = Album_Id
+            };
+            ourDatabase.Add(photo);
+            ourDatabase.SaveChanges();
+            return photo;
+        }
     }
 }
