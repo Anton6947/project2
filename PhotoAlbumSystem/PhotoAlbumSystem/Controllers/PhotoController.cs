9dae4fcaf87bb5626c7def7ddc3b03bcee76d43b
CottonEyeJoey <joedel1310@gmail.com>
2021-11-07 13:06:59 +0200

    Added controller classes

---
+++ PhotoAlbumSystem/PhotoAlbumSystem/Controllers/PhotoController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoAlbumSystem.Controllers
+{
+    public class PhotoController
+    {
+    }
+}
