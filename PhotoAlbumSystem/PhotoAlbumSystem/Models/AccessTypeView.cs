d2b4e54826bc05835a6e56c1e35bf4c61875c6dd
CottonEyeJoey <joedel1310@gmail.com>
2021-11-17 11:36:09 +0200

    Created view models for Entities

---
+++ PhotoAlbumSystem/PhotoAlbumSystem/Models/AccessTypeView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoAlbumSystem.Models
+{
+    public class AccessTypeView
+    {
+
+        public string Name { get; set; }
+
+    }
+}
---
+++ PhotoAlbumSystem/PhotoAlbumSystem/Models/AlbumAccessView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoAlbumSystem.Models
+{
+    public class AlbumAccessView
+    {
+
+        public DateTime Date { get; set; }
+
+        public string User_Id { get; set; }
+       
+    }
+}
