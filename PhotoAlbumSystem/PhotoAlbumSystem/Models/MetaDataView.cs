22365b8552b4c474679698fba9a42d46fe7e4b83
Anton <abuitendag1999@gmail.com>
2021-11-17 11:35:46 +0200

    Created views for Entities

---
+++ PhotoAlbumSystem/PhotoAlbumSystem/Models/MetaDataView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoAlbumSystem.Models
+{
+    public class MetaDataView
+    {
+        
+
+        public string GeoLocation { get; set; }
+        public string Tags { get; set; }
+        public DateTime CapturedDate { get; set; }
+        public string CapturedByUser_Id { get; set; }
+
+       
+    }
+}
---
+++ PhotoAlbumSystem/PhotoAlbumSystem/Models/PhotoAccessView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoAlbumSystem.Models
+{
+    public class PhotoAccessView
+    {
+
+
+
+        public DateTime Date { get; set; }
+
+        public string User_Id { get; set; }
+
+
+
+
+    }  
+}
